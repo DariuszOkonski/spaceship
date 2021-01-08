edafc994ebf1007be594a17607b7f5c1809d51a6
Dariusz Okonski <darek200180@gmail.com>
2021-01-08 20:53:38 +0000

    make missile not leave on the right and left edge of the browser

--- js/Spaceship.js
+++ js/Spaceship.js
@@ -1,7 +1,7 @@
 import { Missile } from './Missile.js';
 export class Spaceship {
     missiles = [];
-    #modifier = 5;
+    #modifier = 10;
     #leftArrow = false;
     #rightArrow = false;
 
@@ -58,11 +58,11 @@ export class Spaceship {
     }
 
     #whatKey() {
-        if(this.#leftArrow && this.#getPosition() > 0) {
+        if(this.#leftArrow && this.#getPosition() > 12) {
             this.element.style.left = `${parseInt(this.element.style.left, 10) - this.#modifier}px`;
         }
 
-        if(this.#rightArrow && this.#getPosition() < window.innerWidth) {
+        if(this.#rightArrow && this.#getPosition() + 12 < window.innerWidth) {
             this.element.style.left = `${parseInt(this.element.style.left, 10) + this.#modifier}px`;
         }
     }
